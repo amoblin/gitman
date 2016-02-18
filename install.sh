bf9d4d294286134b8972ad2d1feb2c8370044c4f
amoblin@mbp <amoblin@gmail.com>
2016-02-18 20:25:46 +0800

    add install information

--- install.sh
+++ install.sh
@@ -9,12 +9,45 @@
 #name=`basename "$1"`
 #name_without_extension=`echo ${name%.*}`
 
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
+# Only enable exit-on-error after the non-critical colorization stuff,
+# which may fail on systems lacking tput or terminfo
+set -e
+
+umask g-w,o-w
+
+printf "${BLUE}Cloning gitdisk...${NORMAL}\n"
 hash git >/dev/null 2>&1 || {
     echo "Error: git is not installed"
     exit 1
 }
 
 TMP_PATH=/tmp/gitdisk
+
+if [ -d $TMP_PATH ]; then
+    rm -rf $TMP_PATH
+fi
 
 env git clone --depth=1 https://github.com/amoblin/gitdisk $TMP_PATH || {
     printf "Error: git clone of gitdisk repo failed\n"
@@ -23,9 +56,17 @@ env git clone --depth=1 https://github.com/amoblin/gitdisk $TMP_PATH || {
 
 cd $TMP_PATH
 
+printf "${BLUE}Installing scripts...${NORMAL}\n"
+
 if [ ! -d ~/bin ]; then
     mkdir ~/bin
 fi
 
 cp gitdisk-update ~/bin
 sudo cp gitdisk-update.applescript "/Library/Scripts/Folder Action Scripts"
+
+printf "${GREEN}"
+echo ''
+echo 'Succeed! Done!'
+echo ''
+  printf "${NORMAL}"
